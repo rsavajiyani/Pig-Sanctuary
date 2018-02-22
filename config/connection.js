c33d99666145382ecb56ecdd8aeccfb7624ac23b
Ravi <rsavaj@gmail.com>
2018-02-22 14:12:49 -0700

    testing connection

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,23 @@
 //test
+var mysql = require('mysql');
+var connection;
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'pig_sanctuary_db'
+    });
+};
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
