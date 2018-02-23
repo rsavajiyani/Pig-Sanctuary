74b1012045c8a88420c207c327abf5245f5aee83
Ravi <rsavaj@gmail.com>
2018-02-23 10:33:24 -0700

    test merge

---
+++ models/pigs.js
@@ -0,0 +1,58 @@
+module.exports = function (sequelize, DataTypes) {
+    var Pigs = sequelize.define("Pigs", {
+        pigName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigAge: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigGender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigImage: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigColor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigBio: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        pigName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        isAdopted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    return Pigs;
+};
